a80f27ae36fdc349b35e71a2ad84d0d44079c32f
Maija <hello@maijagrudule.com>
2023-06-20 16:25:34 +0100

    Added application form links

--- app.config.ts
+++ app.config.ts
@@ -20,8 +20,24 @@ export default defineAppConfig({
     },
     tickets: { title: 'Register now', href: 'https://www.eventbrite.com/' },
     application_hacker: {
-      title: 'Apply for a spot',
-      href: 'https://www.typeform.com/',
+      title: 'Apply to hack',
+      href: 'https://docs.google.com/forms/d/1DCS5xhkOl0-OuyWbNsC8MW0Si6CneYBXODNaSZBNmfM/viewform',
+    },
+    application_speaker: {
+      title: 'Apply to speak',
+      href: 'https://docs.google.com/forms/d/1WsKb_Xf-O1m771wxo7yahDPlUwoAyHjb7BojDwVrRHM/viewform',
+    },
+    application_volunteer: {
+      title: 'Apply to volunteer',
+      href: 'https://docs.google.com/forms/d/1pLeCshKOxGUiXwbINxYyv2ZusqH8ElgxD2QJut3lXzA/viewform',
+    },
+    application_mentor: {
+      title: 'Apply to mentor',
+      href: 'https://docs.google.com/forms/d/14RJ1A7JUTEMHZ0rPfCv4LZ7mdXDOKhsdxONDmgrSr9Y/viewform',
+    },
+    application_media: {
+      title: 'Media application',
+      href: 'https://docs.google.com/forms/d/1cpBc_SAhJnHRwQELrD6Ivy8Ljo6t_NV_DEnlQBjJn2s/viewform',
     },
     hacker_manual: { title: 'Hackers Manual', href: 'https://' },
     sponsor_manual: { title: 'Sponsors Manual', href: 'https://}' },
