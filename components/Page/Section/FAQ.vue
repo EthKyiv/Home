68d4ba84667dcf3201ea868ccb797f59258d262e
Maija <hello@maijagrudule.com>
2023-06-23 16:17:44 +0100

    Added agenda section & refactored section titles

--- components/Page/Section/FAQ.vue
+++ components/Page/Section/FAQ.vue
@@ -3,29 +3,24 @@ import { AppConfigInput } from '@nuxt/schema'
 const app = useAppConfig() as AppConfigInput
 </script>
 <template>
-  <PageSection
-    id="FAQ"
-    class="flex-1 flex flex-col items-start justify-center mt-10 py-5"
-  >
-    <div>
-      <h2
-        class="text-gray-800 dark:text-slate-300 font-bold capitalize text-3xl"
-      >
-        Frequently Asked Questions
-      </h2>
-      <p class="mt-4 text-base">
+  <PageSection id="FAQ">
+    <PageSectionTitle title="Frequently Asked Questions">
+      <template #before>
+        <BrandSkyline class="h-48 w-auto text-primary-500 mb-8" />
+      </template>
+      <p>
         Can't find the answer you're looking for?<br />Read the
         <Anchor class="font-bold" v-bind="app.links.hacker_manual"
           >Hackers Guide</Anchor
         >
         or visit our
-
         <Anchor class="font-bold" v-bind="app.links.discord_support"
           >#member-support</Anchor
         >
         on Discord channel to ask for help.
       </p>
-    </div>
+    </PageSectionTitle>
+
     <FAQ></FAQ>
   </PageSection>
 </template>
