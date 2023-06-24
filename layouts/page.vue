c43ed92cff46c87df20a7a8cca4277c5bd3cf14c
Maija <hello@maijagrudule.com>
2023-06-24 16:46:02 +0100

    Added comments to page layout

--- layouts/page.vue
+++ layouts/page.vue
@@ -2,17 +2,18 @@
   <slot name="app-before" />
   <div id="app-before"></div>
   <div class="flex flex-col min-h-screen relative">
+<!-- navbar slot  -->
     <slot name="header">
       <PageNavbar />
     </slot>
 
     <div class="flex-1 w-full flex flex-col">
-      <div
-        class="relative flex-1 flex flex-col mx-auto max-w-100vw lg:max-w-8xl min-h-100vh"
-      >
+      <div class="relative flex-1 flex flex-col mx-auto max-w-100vw lg:max-w-8xl min-h-100vh">
+<!-- body slot  -->
         <slot></slot>
       </div>
     </div>
+    <!-- footer slot  -->
     <slot name="footer">
       <PageFooter />
     </slot>
