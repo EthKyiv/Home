d5d1082b2925e59c1b24a686170293ed9f87a2ac
Maija <hello@maijagrudule.com>
2023-07-07 00:18:33 +0300

    Adjusted responsivness day zero col

--- pages/index.vue
+++ pages/index.vue
@@ -149,24 +149,24 @@ definePageMeta({
 
       <PageSection
         id="about"
-        class="flex-1 flex flex-col items-start justify-center mt-10 py-5"
+        class="flex-1 flex flex-col items-start justify-center mt-10 py-5 "
       >
         <ContentQuery v-slot="{ data }" path="page/section/dayzero" find="one">
           <h2 class="section-title" data-aos="fade-up">
             {{ data.title }}
           </h2>
 
-          <div class="mt-10 ml-auto grid md:grid-cols-2 gap-4 md:gap-16">
-            <div data-aos="fade-up">
+          <div class="mt-10 ml-auto grid md:grid-cols-6 lg:grid-cols-2 gap-4 md:gap-16">
+            <div class="md:col-span-5 lg:col-span-1 mb-10 md:mb-0" data-aos="fade-up" >
               <ContentRenderer
                 :value="data"
-                class="prose leading-tight text-gray-800 dark:text-slate-300 max-w-none"
+                class="prose leading-tight text-gray-800 dark:text-slate-300 max-w-none bg-white/[0.8]"
               >
                 <template #empty></template>
               </ContentRenderer>
             </div>
             <div
-              class="bg-gray-100 aspect-video"
+            class="md:col-span-5 lg:col-span-1" 
               data-aos="fade-up"
               data-aos-delay="400"
             >
